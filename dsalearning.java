bf4452a65b5bd4ccbc176104b8b3988a238ba016
Aditya <workwith.adityapatil@gmail.com>
2025-07-22 23:58:11 +0530

    pushing code second time today

--- dsalearning.java
+++ dsalearning.java
@@ -2,6 +2,11 @@ public class dsalearning {
 
     public static void main(String[] args) {
         System.out.println("Push My Code In My Github");
+        int a = 5;
+        int b = 5;
+        int num = a*b;
+        System.out.println(num);
+        
 
     }
 }
