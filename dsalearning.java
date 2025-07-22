09f65fb71f776479d7eaf61bbeab24a57d0e23e1
Aditya <workwith.adityapatil@gmail.com>
2025-07-22 23:29:32 +0530

    Pushing my first code

--- dsalearning.java
+++ dsalearning.java
@@ -1,11 +1,7 @@
 public class dsalearning {
 
     public static void main(String[] args) {
-        System.out.println("first java file ");
-        int a=10;
-        int b=5;
-        System.out.println(a+b);
-        //new comment;
+        System.out.println("Push My Code In My Github");
 
     }
 }
