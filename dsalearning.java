e360f62aeeab48867ca577d6463a95f17d453004
Aditya <workwith.adityapatil@gmail.com>
2025-07-22 19:57:45 +0530

    2nd content added

--- dsalearning.java
+++ dsalearning.java
@@ -2,5 +2,9 @@ public class dsalearning {
 
     public static void main(String[] args) {
         System.out.println("first java file ");
+        int a=10;
+        int b=5;
+        System.out.println(a+b);
+        
     }
 }
