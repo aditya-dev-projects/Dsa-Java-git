e20a98bc382b674297f9c76a77756199b9b325aa
Aditya <workwith.adityapatil@gmail.com>
2025-07-25 02:29:16 +0530

    solved factorial question

--- functions.java
+++ functions.java
@@ -1,12 +1,30 @@
+import java.util.*;
 public class functions{ 
     public static int multiply(int a,int b){
         int product = a*b;
         return product;
     }
+
+    // Find the factorial 0f n (take input n)
+    
+        public static int factorial(int n){
+            int f = 1;
+            for (int i = 1; i <= n; i++) {
+                f = f*i;
+                
+            }
+            return f;
+        }
+    
     public static void main(String[] args) {
-        int product = multiply(3,5);
-        System.out.println("Product is:"+product);
+        System.out.println("Enter the value of n");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(factorial(n));
+        
+
     }
     
 }
+
 
